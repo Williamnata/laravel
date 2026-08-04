82004d9e4051a9605b8278b4cbe0068679300a44
William Nata Silva Mascarenhas <william29370726@aluno.ba.senac.br>
2026-08-03 21:10:23 -0300

    Aula 03/08/2026 Laravel

---
+++ resources/views/dashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <form method="post">
+            <div>
+                <h1>Descubra seu dashboard</h1>
+              
+            </div>
+
+        </form>
+    </div>
+  
+</body>
+
+</html>
---
+++ resources/views/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <form method="post">
+            <div>
+                <h1>Descubra seu peso</h1>
+                <p>Insira seu peso</p>
+                <input type="text" name="peso">
+                <p>Insira sua altura</p>
+                <input type="text" name="altura">
+                <button type="submit">Calcular</button>
+            </div>
+
+        </form>
+    </div>
+  
+</body>
+
+</html>
