cd46fc63aca811d47fd41876b235525c58102284
William Nata Silva Mascarenhas <william29370726@aluno.ba.senac.br>
2026-08-18 20:12:02 -0300

    cv

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+       $middleware->alias([
+        'auth.nivel ' => \App\Http\Middleware\Autentication::class,
+       ]);
+       
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
